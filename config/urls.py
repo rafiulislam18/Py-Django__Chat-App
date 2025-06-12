ef26cb0105660706de4ba4d74a7d312c3c3e4e8b
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2025-06-12 22:19:51 +0600

    config(config): update project configurations

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from apps.users.views import profile_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', include('apps.rtchat.urls')),
+    path('profile/', include('apps.users.urls')),
+    path('@<username>/', profile_view, name="profile"),
 ]
+
+# Only used when DEBUG=True, whitenoise can serve files when DEBUG=False
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
