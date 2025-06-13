1e9c70976d0cf91a61571663a00d029635a10b42
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2025-06-13 17:58:23 +0600

    fix(config): import error fixed in config.asgi

--- config/asgi.py
+++ config/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security import AllowedHostsOriginValidator
+from channels.security.websocket import AllowedHostsOriginValidator
 from channels.auth import AuthMiddlewareStack
 
 
