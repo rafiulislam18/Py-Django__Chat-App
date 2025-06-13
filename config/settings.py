1aa0fd0b1497e2cff34987a3a9dd058b9ee00630
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2025-06-13 19:18:23 +0600

    feat(apps/rtchat): complete real-time chat with multi-user support via channel layers

--- config/settings.py
+++ config/settings.py
@@ -95,11 +95,11 @@ TEMPLATES = [
 
 ASGI_APPLICATION = 'config.asgi.application'
 
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels.layers.InMemoryChannelLayer",
-#     },
-# }
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    },
+}
 
 
 # Database
